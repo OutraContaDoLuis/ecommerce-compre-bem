24798de1ec96ab56c1782688c9237bf91e549ce2
unknown <luis.henri.campaner@gmail.com>
2025-11-30 02:54:35 -0300

    Dando inicio ao backend do projeto

---
+++ comprebem/src/main/java/br/com/luis/comprebem/ComprebemApplication.java
@@ -0,0 +1,13 @@
+package br.com.luis.comprebem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComprebemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComprebemApplication.class, args);
+	}
+
+}
---
+++ comprebem/src/test/java/br/com/luis/comprebem/ComprebemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luis.comprebem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComprebemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
